96718c7cb019b923e0ce2c7cb8d71c67248b1be4
Vanessa Spear <vanessavspear@gmail.com>
2023-03-10 11:04:24 -0600

    Created an authentication view to handle user login and registration.

--- passport/urls.py
+++ passport/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from passportapi.views import register_user, login_user
 
 urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
