1a1ccf0c501a6171fcc0865337b1d8cc1bb8f8d5
Vanessa Spear <vanessavspear@gmail.com>
2023-03-21 10:02:05 -0500

    Added a custom action to trip view to get all past trips for a user

--- passportapi/views/trip_view.py
+++ passportapi/views/trip_view.py
@@ -129,6 +129,17 @@ class TripView(ViewSet):
             user=user, departure_date__gt=date.today())
         return Response(upcoming_trips.count(), status=status.HTTP_200_OK)
     
+    @action(methods=['get'], detail=False)
+    def past(self, request):
+        """Determines the number of past trips for the user
+        @api {GET} /trips/past GET past trip count
+        """
+        user=request.auth.user
+        past_trips = Trip.objects.filter(
+            user=user, departure_date__lte=date.today())
+        serializer = TripSerializer(past_trips, many=True)
+        return Response(serializer.data)
+    
         
 class ReasonSerializer(serializers.ModelSerializer):
     class Meta:
