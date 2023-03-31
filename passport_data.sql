78a1a345c9023def1c18b11eb8a53d8ff3de8825
Vanessa Spear <vanessavspear@gmail.com>
2023-03-31 10:15:20 -0500

    Added gifs demo'ing user experience

--- passport_data.sql
+++ passport_data.sql
@@ -1,3 +1,18 @@
 UPDATE `passportapi_stampphoto` 
 SET `date_created` = '2019-02-24 23:13:27.078571'
-WHERE `id` = 27;
+WHERE `id` = 29;
+
+DELETE FROM `passportapi_stampphoto` 
+WHERE `id` = 31;
+
+UPDATE `passportapi_stampproduct` 
+SET `description` = 'I used these boots hiking in the Taroko national forest, and my feet were so comfy all day! Bonus perk: They''re cute!',
+`date_created` = '2022-12-28 23:13:27.078571'
+WHERE `id` = 1;
+
+UPDATE `passportapi_stampjournal` 
+SET `name` = 'Visiting Taroko National Park', 
+`entry` = 'We went on a challenging yet unforgettable hiking adventure in Taroko National Park, Taiwan. The park is located in the mountainous region of Taiwan and is known for its stunning cliffs, waterfalls, and lush greenery.  As we hiked, we were surrounded by soaring cliffs and the sound of the rushing river. One of the highlights of the hike was the Eternal Spring Shrine, which is perched on a cliff with a breathtaking waterfall behind it.',
+`date_created` = '2022-12-27 23:13:27.078571'
+WHERE `id` = 4;
+
